795bba8a8a4241ae6ce469514df316ceab73f48a
Alek Pialucha <pialuchaa@btinternet.com>
2023-10-10 11:57:00 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import CreativeSizeBanner from './components/CreativeSizeBanner'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [creativeSizes, setCreativeSizes] = useState(["100x300", "450x450"])
+  return (
+    <>
+      <CreativeSizeBanner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CreativeSizeBanner.tsx
@@ -0,0 +1,41 @@
+import { FormEvent, useState, useRef } from "react"
+
+const CreativeSizeBanner = () => {
+
+  const [creativeSizes, setCreativeSizes] = useState(["100x300", "450x450"])
+  const creativeSizeHeightRef = useRef<HTMLInputElement>(null)
+  const creativeSizeWidthRef = useRef<HTMLInputElement>(null)
+
+  const addNewCreativeHandler = (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newHeight = creativeSizeHeightRef.current?.value;
+    const newWidth = creativeSizeWidthRef.current?.value;
+
+    if (newHeight && newWidth) {
+      const newSize = `${newHeight}x${newWidth}`;
+      setCreativeSizes([...creativeSizes, newSize]);
+
+      // Clear input fields after adding new creative size
+      creativeSizeHeightRef.current.value = "";
+      creativeSizeWidthRef.current.value = "";
+    }
+  };
+
+  return (
+    <div className="creativeSizeContainer">
+      <ul className="creativeSizeList">
+        {creativeSizes.map((size) => {
+          return <li key={size} className="creativeSize">{size}</li>
+        })}
+        <li className="addNewCreativeSize">+</li>
+        <form onSubmit={addNewCreativeHandler}>
+          <input ref={creativeSizeHeightRef} type="text" placeholder="Height"></input>
+          <input ref={creativeSizeWidthRef} type="text" placeholder="Width"></input>
+          <button type="submit">Add</button>
+        </form>
+      </ul>
+    </div>
+  )
+}
+
+export default CreativeSizeBanner
