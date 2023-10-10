6385efb714d35268c5613a879333139503572249
Alek Pialucha <pialuchaa@btinternet.com>
2023-10-11 00:28:43 +0100

    Campaign Selector functionality

--- src/components/CampaignSelector/CampaignSelector.tsx
+++ src/components/CampaignSelector/CampaignSelector.tsx
@@ -1,24 +1,36 @@
+import { FormEvent, useRef } from "react";
+
 interface ICampaignSelector {
   campaigns: string[];
   selectedCampaign: string;
   onSelectedCampaign: (campaign: string) => void;
 }
-
 const CampaignSelector = ({
   campaigns,
   selectedCampaign,
   onSelectedCampaign,
 }: ICampaignSelector) => {
+  const campaignRef = useRef<HTMLInputElement>(null);
+  const changeCampaignHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const selectedValue = campaignRef.current?.value;
+    if (selectedValue) {
+      onSelectedCampaign(selectedValue);
+    }
+  };
+
   return (
     <>
-      <h2>{selectedCampaign}</h2>
-      <form action="">
-        <input list="campaigns" name="campaign" />
+      <h2 className="campaignTitle">{selectedCampaign}</h2>
+      <p className="changeSelection">Change Selection</p>
+      <form className="campaignForm" onSubmit={changeCampaignHandler}>
+        <input ref={campaignRef} list="campaigns" name="campaign" />
         <datalist id="campaigns">
           {campaigns.map((name) => {
             return <option key={name} value={name}></option>;
           })}
         </datalist>
+        <button type="submit">Select</button>
       </form>
     </>
   );
