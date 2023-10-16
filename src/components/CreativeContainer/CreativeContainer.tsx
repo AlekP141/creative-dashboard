86d2bab6fd42f402aa422a2b393d80a16d6f5811
Alek Pialucha <pialuchaa@btinternet.com>
2023-10-16 01:13:43 +0100

    initial 300x600 porsche ad work

--- src/components/CreativeContainer/CreativeContainer.tsx
+++ src/components/CreativeContainer/CreativeContainer.tsx
@@ -7,22 +7,31 @@ interface ICreativeContainer {
   selectedSize: string;
 }
 
-const fileValidation = async (size: string, campaign: string): Promise<boolean> => {
+const fileValidation = async (
+  size: string,
+  campaign: string
+): Promise<boolean> => {
   try {
-    const response = await fetch(`/campaigns/${campaign}/${size}-${campaign}.html`);
+    const response = await fetch(
+      `/campaigns/${campaign}/${size}-${campaign}.html`
+    );
     return response.status === 200;
   } catch (error) {
-    console.error('Error fetching file:', error);
+    console.error("Error fetching file:", error);
     return false;
   }
 };
 
-const CreativeContainer = ({ children, selectedCampaign, selectedSize }: ICreativeContainer) => {
+const CreativeContainer = ({
+  children,
+  selectedCampaign,
+  selectedSize,
+}: ICreativeContainer) => {
   const [isValidFile, setIsValidFile] = useState(false);
 
   useEffect(() => {
     const validateFile = async () => {
-      if (selectedSize){
+      if (selectedSize) {
         const isValid = await fileValidation(selectedSize, selectedCampaign);
         setIsValidFile(isValid);
       }
@@ -31,37 +40,38 @@ const CreativeContainer = ({ children, selectedCampaign, selectedSize }: ICreati
     validateFile();
   }, [selectedCampaign, selectedSize]);
 
-
   // Test case for how to work the CSS options tab
   useEffect(() => {
-    {console.log(`/campaigns/${selectedCampaign}/${selectedSize}-${selectedCampaign}.html`)}
     setTimeout(() => {
-      const iframe = document.getElementById("iframe") as HTMLIFrameElement
+      const iframe = document.getElementById("iframe") as HTMLIFrameElement;
       // Check if the iframe is loaded
       if (iframe && iframe.contentDocument) {
         const iframeDocument = iframe.contentDocument;
-        const elementInsideIframe = iframeDocument.querySelector('.test') as HTMLElement;
+        const elementInsideIframe = iframeDocument.querySelector(
+          ".test"
+        ) as HTMLElement;
 
         // Modify CSS properties of the element inside the iframe
         if (elementInsideIframe) {
-          elementInsideIframe.style.color = 'red';
-          elementInsideIframe.style.fontSize = '20px';
+          elementInsideIframe.style.color = "red";
+          elementInsideIframe.style.fontSize = "20px";
         }
       }
     }, 100);
   }, [selectedSize]);
 
-
-
   return (
     <div className="creativeFrame">
       {children}
       <div className="creativeContainer">
-      {isValidFile ? (
-        <iframe id="iframe" src={`/campaigns/${selectedCampaign}/${selectedSize}-${selectedCampaign}.html`} />
+        {isValidFile ? (
+          <iframe
+            id="iframe"
+            src={`/campaigns/${selectedCampaign}/${selectedSize}-${selectedCampaign}.html`}
+          />
         ) : (
           <h2>Error: No file found in this size.</h2>
-          )}
+        )}
       </div>
     </div>
   );
