a8652629c27f35c1f060cb3fa171db87cab1271b
Alek Pialucha <pialuchaa@btinternet.com>
2023-10-18 21:09:27 +0100

    class name extraction from iframe

--- src/components/CreativeOptions/CreativeOptions.tsx
+++ src/components/CreativeOptions/CreativeOptions.tsx
@@ -1,27 +1,54 @@
 import "./CreativeOptions.scss";
-import { useEffect, useState, useLayoutEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 const CreativeOptions = () => {
   const [creativeHeight, setCreativeHeight] = useState<number>(0);
   const [creativeWidth, setCreativeWidth] = useState<number>(0);
   const iframe = document.getElementById("iframe") as HTMLIFrameElement;
-  const creativeRef = useRef<Document | null>(null);
+  let creative: Document | null = null;
   if (iframe && iframe.contentDocument) {
-    creativeRef.current = iframe.contentDocument;
+    creative = iframe.contentDocument;
   }
 
+  interface IClassNames {
+    [key: number]: string[]
+  }
 
+  const extractClassNames = (element: HTMLElement, classNames: IClassNames = {}, hashValue = 0) => {
 
-  useLayoutEffect(() => {
-    const creative = creativeRef.current;
+    console.dir(element.classList)
+    console.log(typeof(element.className))
+    if (element && element.classList.length != 0) {
+      const classes = Array.from(element.classList)
+      classNames[hashValue] = classes;
+    }
+
+    if (element && element.children) {
+      const childrenArray = Array.from(element.children);
+      const keys = Object.keys(classNames);
+      hashValue = Number(keys[keys.length - 1]) + 1;
+
+      childrenArray.forEach((el) => {
+        classNames = extractClassNames(el as HTMLElement, classNames, hashValue);
+        hashValue++;
+      });
+    }
+
+    return classNames;
+  };
+
+  useEffect(() => {
     if (creative) {
       const body = creative.querySelector("body") as HTMLBodyElement;
       const newHeight = body.offsetHeight - 2;
       const newWidth = body.offsetWidth - 2;
       setCreativeHeight(newHeight);
       setCreativeWidth(newWidth);
+
+      const backgroundImage = creative.querySelector(".backgroundImage") as HTMLElement
+      console.log(extractClassNames(backgroundImage))
     }
-  }, []);
+  }, [creative]);
 
   useEffect(() => {
     console.log(creativeHeight);
@@ -31,6 +58,9 @@ const CreativeOptions = () => {
   return (
     <div className="optionsContainer">
       <h2 className="optionsTitle">Options</h2>
+      <form action="">
+        <h4></h4>
+      </form>
     </div>
   );
 };
