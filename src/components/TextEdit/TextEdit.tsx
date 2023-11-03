1c509e2bfabcffcdb7c512d92286228be6651926
Alek Pialucha <pialuchaa@btinternet.com>
2023-11-03 11:48:52 +0000

    text component initial commit

---
+++ src/components/TextEdit/TextEdit.tsx
@@ -0,0 +1,15 @@
+import selectCreative from "../../functions/selectCreative"
+
+
+const TextEdit = () => {
+  const creative = selectCreative()
+  
+
+  return (
+    <div className="textEdit">
+
+    </div>
+  )
+}
+
+export default TextEdit
