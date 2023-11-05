9efd0c92b3ac7df97e395ff202d667c975ec00e5
Alek Pialucha <pialuchaa@btinternet.com>
2023-11-05 22:49:04 +0000

    issue with retrieving text style

--- src/components/TextEdit/TextEdit.tsx
+++ src/components/TextEdit/TextEdit.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import selectCreative from "../../functions/selectCreative";
 
 interface ITextEdit {
@@ -8,7 +9,43 @@ const TextEdit = ({ classes }: ITextEdit) => {
   const creative = selectCreative();
   const targetClass = classes[0];
   console.log(targetClass);
-  return <div className="textEdit"></div>;
+  const [left, setLeft] = useState("");
+  const [fontSize, setfontSize] = useState("");
+  const [top, setTop] = useState("");
+  const [text, setText] = useState("");
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    if (creative) {
+      const targetElement = creative.querySelector(`.${classes[0]}`) as HTMLElement;
+      console.log(targetElement)
+      setLeft(targetElement.style.left)
+      setfontSize(targetElement.style.fontSize)
+      setTop(targetElement.style.top)
+      setText(targetElement.innerText)
+      setColor(targetElement.style.color);
+    }
+  }, [creative, classes])
+
+  useEffect(() => {
+    console.log(left)
+    console.log(fontSize)
+    console.log(top)
+    console.log(text)
+    console.log(color)
+  }, [left, fontSize, top, text, color])
+
+  if (creative) {
+    const targetElement = creative.querySelector(`.${classes[0]}`) as HTMLElement;
+  }
+
+  return <div className="textEdit">
+    <p>{left}</p>
+    <p>{fontSize}</p>
+    <p>{top}</p>
+    <p>{text}</p>
+    <p>{color}</p>
+  </div>;
 };
 
 export default TextEdit;
