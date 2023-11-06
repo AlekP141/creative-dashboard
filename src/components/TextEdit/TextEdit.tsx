2605c9cc48cc85b6d4331c4996b00a5d5a6291ac
Alek Pialucha <pialuchaa@btinternet.com>
2023-11-06 14:50:35 +0000

    text align, left, top + text content options complete

--- src/components/TextEdit/TextEdit.tsx
+++ src/components/TextEdit/TextEdit.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import selectCreative from "../../functions/selectCreative";
 import "./TextEdit.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAlignLeft,
+  faAlignCenter,
+  faAlignRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 interface ITextEdit {
   classes: string;
@@ -13,56 +19,157 @@ const TextEdit = ({ classes, width, height }: ITextEdit) => {
   const targetClass = classes[0];
   // console.log(targetClass);
   const [left, setLeft] = useState<number>();
-  const [top, setTop] = useState("");
+  const [top, setTop] = useState<number>();
   const [fontSize, setfontSize] = useState("");
   const [text, setText] = useState("");
+  const [newText, setNewText] = useState<string>();
   const [color, setColor] = useState("");
+  const [textAlign, setTextAlign] = useState("left");
 
+  // Set initial values
   useEffect(() => {
     if (creative) {
       const targetElement = creative.querySelector(
         `.${targetClass}`
       ) as HTMLElement;
-      const targetElementStyle = getComputedStyle(targetElement)
+      const targetElementStyle = getComputedStyle(targetElement);
+      console.log(targetElement.innerText);
       setLeft(parseInt(targetElementStyle.left, 10));
       setfontSize(targetElementStyle.fontSize);
-      setTop(targetElementStyle.top);
+      setTop(parseInt(targetElementStyle.top, 10));
       setText(targetElement.innerText);
       setColor(targetElementStyle.color);
       // console.log(targetElement.innerText)
     }
   }, [creative, classes]);
 
+  // Update all values on any change
   useEffect(() => {
     const targetElement = creative?.querySelector(
       `.${classes[0]}`
     ) as HTMLElement;
     // console.log(targetElement)
-    targetElement.style.left=`${left}px`
-    targetElement.style.top=`${top}px`
-    targetElement.style.fontSize=`${fontSize}px`
-    targetElement.style.color=text
-    targetElement.style.color=`#${color}`
-  }, [left, fontSize, top, text, color]);
+    targetElement.style.left = `${left}px`;
+    targetElement.style.top = `${top}px`;
+    targetElement.style.fontSize = `${fontSize}px`;
+    if (newText != undefined) {
+      targetElement.innerText = newText;
+    }
+    targetElement.style.color = `#${color}`;
+    targetElement.style.textAlign = textAlign;
+    // console.log(textAlign)
+  }, [creative, classes, left, fontSize, top, newText, color, textAlign]);
 
-  if (creative) {
-    const targetElement = creative.querySelector(
-      `.${classes[0]}`
-    ) as HTMLElement;
+
+  interface ISliderChangeHandler {
+    direction: string;
   }
 
-  const sliderChangeHandler = () => {
-    const rangeSlider = document.querySelector(`#${classes[0]}Range`) as HTMLInputElement;
-    setLeft(parseInt(rangeSlider.value))
-  }
+  const sliderChangeHandler = ({ direction }: ISliderChangeHandler) => {
+    const rangeSlider = document.querySelector(
+      `#${classes[0]}${direction}Range`
+    ) as HTMLInputElement;
+    switch (direction) {
+      case "Left":
+        setLeft(parseInt(rangeSlider.value));
+        break;
+      case "Top":
+        setTop(parseInt(rangeSlider.value));
+    }
+  };
+
+  const textChangeHandler = () => {
+    const textArea = document.querySelector(
+      `#${classes[0]}TextArea`
+    ) as HTMLTextAreaElement;
+    setNewText(textArea.value);
+  };
 
   return (
     <div className="textEdit">
-      <p>{left}px</p>
-      <input className="sliderInputTrack" id={`${classes[0]}Range`} type="range" min="0" max={width} value={left || 0} onChange={sliderChangeHandler}/>
-      <p>{top}</p>
-      <p>{fontSize}</p>
-      <p>{text}</p>
+      <div className="textAlignGroup">
+        <input
+          className="radioInput"
+          type="radio"
+          name={`${classes[0]}TextAlign`}
+          id={`${classes[0]}TextAlignLeft`}
+          value="left"
+          checked={textAlign === "left"}
+          onChange={() => {
+            setTextAlign("left");
+          }}
+        />
+        <label
+          className="textAlignLabel"
+          htmlFor={`${classes[0]}TextAlignLeft`}
+        >
+          <FontAwesomeIcon icon={faAlignLeft} />
+        </label>
+
+        <input
+          className="radioInput"
+          type="radio"
+          name={`${classes[0]}TextAlign`}
+          id={`${classes[0]}TextAlignCenter`}
+          value="center"
+          checked={textAlign === "center"}
+          onChange={() => {
+            setTextAlign("center");
+          }}
+        />
+        <label
+          className="textAlignLabel"
+          htmlFor={`${classes[0]}TextAlignCenter`}
+        >
+          <FontAwesomeIcon icon={faAlignCenter} />
+        </label>
+
+        <input
+          className="radioInput"
+          type="radio"
+          name={`${classes[0]}TextAlign`}
+          id={`${classes[0]}TextAlignRight`}
+          value="right"
+          checked={textAlign === "right"}
+          onChange={() => {
+            setTextAlign("right");
+          }}
+        />
+        <label
+          className="textAlignLabel"
+          htmlFor={`${classes[0]}TextAlignRight`}
+        >
+          <FontAwesomeIcon icon={faAlignRight} />
+        </label>
+      </div>
+
+      <p>Left Position: {left}px</p>
+      <input
+        className="sliderInputTrack"
+        id={`${classes[0]}LeftRange`}
+        type="range"
+        min="0"
+        max={width}
+        value={left || 0}
+        onChange={() => sliderChangeHandler({ direction: "Left" })}
+      />
+      <p>Top Position: {top}px</p>
+      <input
+        className="sliderInputTrack"
+        id={`${classes[0]}TopRange`}
+        type="range"
+        min="0"
+        max={height}
+        value={top || 0}
+        onChange={() => sliderChangeHandler({ direction: "Top" })}
+      />
+      <p>Font Size: {fontSize}</p>
+      <p>Text:</p>
+      <textarea
+        id={`${classes[0]}TextArea`}
+        value={newText === undefined ? text : newText}
+        onChange={() => textChangeHandler()}
+      />
       <p>{color}</p>
     </div>
   );
