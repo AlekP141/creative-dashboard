0bf66198ed85cfaaa5f09c4593721cc16ad67caa
Alek Pialucha <pialuchaa@btinternet.com>
2023-12-04 15:09:59 +0000

    vite deploy attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import VitePluginPages from 'vite-plugin-pages';
-import VitePluginGhPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePluginPages(),
-    VitePluginGhPages()],
+  base:'/creative-dashboard/',
+  plugins: [react()],
 })
